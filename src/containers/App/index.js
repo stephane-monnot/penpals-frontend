17ea9494d108357dca61275abcc83ec58d43a1d0
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-12 00:34:12 +0100

    Add login screen

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import HomeScreen from '../HomeScreen';
 import AboutScreen from '../AboutScreen';
+import LoginScreen from '../LoginScreen';
 import Navigation from "../../components/Navigation/index";
 import NavLink from "../../components/NavLink/index";
 
@@ -11,12 +12,14 @@ const App = () => (
       <Navigation>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/about-us">About us</NavLink>
+        <NavLink to="/login">Login</NavLink>
       </Navigation>
     </header>
 
     <main>
       <Route exact path="/" component={HomeScreen} />
       <Route exact path="/about-us" component={AboutScreen} />
+      <Route exact path="/login" component={LoginScreen} />
     </main>
   </div>
 );
