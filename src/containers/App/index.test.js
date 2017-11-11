8caaee53c3f552269ae1f0b44e29d6d5dd6e9ef3
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-11 01:54:02 +0100

    Fix some warnings

--- src/containers/App/index.test.js
+++ src/containers/App/index.test.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { StaticRouter } from 'react-router'
+import { StaticRouter } from 'react-router';
 import App from './';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<StaticRouter location="someLocation" context={{}}>
-    <App />
-  </StaticRouter>, div);
+  ReactDOM.render(
+    <StaticRouter location="someLocation" context={{}}>
+      <App />
+    </StaticRouter>, div);
 });
