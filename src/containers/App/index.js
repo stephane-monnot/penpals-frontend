2dd84fd3102be2e94dd8c262c97ebe26cff0ae24
Monnot Stéphane <monnot.stephane@gmail.com>
2017-11-11 23:59:26 +0100

    Add navigation

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import Home from '../Home';
 import About from '../About';
+import Navigation from "../../components/Navigation/index";
+import NavLink from "../../components/NavLink/index";
 
 const App = () => (
   <div>
     <header>
-      <Link to="/">Home</Link>
-      <Link to="/about-us">About</Link>
+      <Navigation>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about-us">About us</NavLink>
+      </Navigation>
     </header>
 
     <main>
